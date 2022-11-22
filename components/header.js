614499497368b5f5bc20a505ee6425042d3373b0
Spencer Sahu <spencer.sahu@pearson.com>
2022-11-22 15:42:36 -0600

    First push with changes

---
+++ components/header.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faLinkedinIn, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+
+export default function Header() {
+  return (
+    <nav class="navbar bg-light">
+        <div class="container-fluid py-0">
+            <div>
+                <Link class="px-2" href="mailto:saheasy@gmail.com">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                </Link>
+                <Link class="px-2" href="https://www.facebook.com/theSahu27/">
+                    <FontAwesomeIcon icon={faFacebook}  />
+                </Link>
+                <Link class="px-2" href="https://www.linkedin.com/in/spencersahu/">
+                    <FontAwesomeIcon icon={faLinkedinIn} />
+                </Link>
+                <Link class="px-2" href="https://github.com/Saheasy">
+                    <FontAwesomeIcon icon={faGithubAlt} />
+                </Link>
+            </div>
+          
+          <div class="d-flex">
+            <Link href="/" class="text-decoration-none px-2">Portfolio</Link>
+            <Link href="/" class="text-decoration-none px-2">Projects</Link>
+          </div>
+        </div>
+      </nav>
+  )
+}
