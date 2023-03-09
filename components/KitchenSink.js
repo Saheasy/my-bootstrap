da8032f9d66d2d25c9b196f601ac6f8d24b46466
Spencer Sahu <saheasy@gmail.com>
2023-03-09 10:58:46 -0600

    changed url and text to KitchenSink for testing

--- components/KitchenSink.js
+++ components/KitchenSink.js
@@ -4,14 +4,14 @@ import ListGroup from 'react-bootstrap/ListGroup';
 function KitchenSinkExample() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Img variant="top" src="holder.js/100px180?text=Imagehttps://www.credly.com/badges/598b60e4-f1c3-4ec9-9275-95acf5f15964/public_url cap" />
       <Card.Body>
         <Card.Title>AWS Cloud Practitioner</Card.Title>
         <Card.Text>
           Gained this certfication to help learn to use AWS service to create and deploy cloud applications.
         </Card.Text>
-        <Card.Link href="https://www.credly.com/badges/598b60e4-f1c3-4ec9-9275-95acf5f15964/public_url">Card Link</Card.Link>
-        <Card.Link href="https://aws.amazon.com/certification/certified-cloud-practitioner/">Another Link</Card.Link>
+        <Card.Link href="https://www.credly.com/badges/598b60e4-f1c3-4ec9-9275-95acf5f15964/public_url">Credly Link</Card.Link>
+        <Card.Link href="https://aws.amazon.com/certification/certified-cloud-practitioner/">Certification Link</Card.Link>
       </Card.Body>
     </Card>
   );
