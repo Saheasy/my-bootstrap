85808a77d99f7c13aa2f4f9e3172b237e40c6742
Spencer Sahu <saheasy@gmail.com>
2023-03-10 09:13:34 -0600

    updated KitchenSink links to open in new tab

--- components/KitchenSink.js
+++ components/KitchenSink.js
@@ -10,8 +10,8 @@ function KitchenSinkExample() {
         <Card.Text>
           Gained this certfication to help learn to use AWS service to create and deploy cloud applications.
         </Card.Text>
-        <Card.Link href="https://www.credly.com/badges/598b60e4-f1c3-4ec9-9275-95acf5f15964/public_url">Credly Link</Card.Link>
-        <Card.Link href="https://aws.amazon.com/certification/certified-cloud-practitioner/">Certification Link</Card.Link>
+        <Card.Link href="https://www.credly.com/badges/598b60e4-f1c3-4ec9-9275-95acf5f15964/public_url" target="_blank" rel="noopener noreferrer">Credly Link</Card.Link>
+        <Card.Link href="https://aws.amazon.com/certification/certified-cloud-practitioner/" target="_blank" rel="noopener noreferrer">Certification Link</Card.Link>
       </Card.Body>
     </Card>
   );
